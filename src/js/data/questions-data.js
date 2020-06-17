e105d6225725665e3d68290cde40ab404f5e4a85
ArmenMesropyan <mesropyan200315@gmail.com>
2020-06-17 23:16:57 +0400

    10 new questions added

--- src/js/data/questions-data.js
+++ src/js/data/questions-data.js
@@ -35,6 +35,78 @@ alert( x++ );
                 answer: 0,
                 choices: ['5', '6', 'number', 'type error'],
             },
+            {
+                question: 'What will be printed?',
+                code: `
+"use strict"
+a = [1, 2, 3];
+b = [1, 2, 3];
+console.log(a == b);
+                `,
+                answer: 2,
+                choices: ['true', 'false', 'error'],
+            },
+            {
+                question: 'What will be alerted?',
+                code: `
+let str = "Hello World!";
+str.something = 2;
+alert(str.something);
+                `,
+                answer: 3,
+                choices: ['Hello World', '2', 'error', 'undefined'],
+            },
+            {
+                question: 'What is the result of this expression?',
+                code: `
+[].push(1,2).unshift(3).join();
+                `,
+                answer: 2,
+                choices: ['3, 1, 2', '3, 1', 'error', 'undefined'],
+            },
+            {
+                question: 'What will be alerted?',
+                code: `
+f.call(f);
+
+function f() {
+    alert( this );
+}
+                `,
+                answer: 0,
+                choices: ['f function code', '[object Object]', 'error', 'window'],
+            },
+            {
+                question: 'Which value will be alerted?',
+                code: `
+function User() { }
+User.prototype = { admin: false };
+
+let user = new User();
+
+User.prototype = { admin: true };
+
+alert(user.admin);
+                `,
+                answer: 1,
+                choices: ['true', 'false', 'undefined'],
+            },
+            {
+                question: 'What happens if you alert true + false?',
+                code: `
+console.log(true + false);
+                `,
+                answer: 2,
+                choices: ['"truefalse"', 'NaN', '1', '0'],
+            },
+            {
+                question: 'What is the result of this expression?',
+                code: `
+[] + false - null + true
+                `,
+                answer: 0,
+                choices: ['"falsenulltrue"', 'NaN', 'error', 'NaNtrue'],
+            },
         ];
     }
 
